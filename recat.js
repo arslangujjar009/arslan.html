fd0a8cd07bcbd719372e769091cc43d120fedba7
Arslan Gujjar <ashirgg604@gmail.com>
2026-05-12 11:18:47 -0700

    Create recat.js

---
+++ recat.js
@@ -0,0 +1,13 @@
+start
+ vs code studio
+      react developer tools
+react folder
+
+First Extension 
+1 thunder client 
+2 es7 react redux
+3 bracket pair colorizer
+4 auto rename tag
+5 live server 
+6 prettier code formatter
+7 previeer
